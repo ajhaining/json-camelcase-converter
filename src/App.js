522be0952a2baccf2b0073e7c31a3a36b5074b7f
Andrew Haining <andrew.haining@responsetap.com>
2019-08-29 13:54:03 +0100

    snakey converter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import camelcaseKeys from "camelcase-keys"
+import beautifyJson from "json-beautify"
 
 function App() {
+  const [blob, setBlob] = useState("")
+  const [error, setError] = useState()
+
+  var camelified = ""
+
+  if (!error && blob) {
+    try {
+      const json = JSON.parse(blob);
+      camelified = beautifyJson(camelcaseKeys(json), null, 2, 80)
+    } catch(e) {
+      setError("JSON is not parseable, check it is valid.")
+    }
+  }
+
+  const handleChange = (value) => {
+    setBlob(value)
+    setError()
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <textarea
+        className="column"
+        onChange={e => handleChange(e.target.value)}
+        placeholder="Insert JSON here."
+      />
+      <div className="column grey">
+        {error ?
+          <div className="error">{error}</div> :
+          <pre>
+            {blob.length > 0 ? camelified : ""}
+          </pre>
+        }
+      </div>
     </div>
   );
 }
